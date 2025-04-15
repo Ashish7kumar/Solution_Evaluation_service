b7f35f184d7d9b3f3a5e869aca0f07ec23c7ae1f
unknown <ashish11to12@gmail.com>
2025-04-15 22:34:16 +0530

    Corrected socket logic by adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 import testCase from "./types/testCase.type";
 import { PORT } from "./Config/server.config";
 const app=express();
-
+app.use(cors());
 const httpServer=createServer(app);
-const io=new Server(httpServer);
+const io=new Server(httpServer, {cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }});
 io.on("connection",(socket)=>{
      console.log("New socket connection Happened Ashish");
      socket.on("evaluate", ({ code, testCases }: { code: string; testCases: testCase[] }) => {
